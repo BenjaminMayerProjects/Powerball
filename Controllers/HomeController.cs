016a2b2883a69618ea7b9734e896548dbed6bb81
benji mayer <benjimayer1@gmail.com>
2023-12-25 20:13:17 -0500

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿// HomeController.cs
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Powerball.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new PowerballModel
+            {
+                RandomNumbers = GenerateRandomNumbers(),
+                UserNumbers = new List<int>()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult CheckNumbers(string userNumbers)
+        {
+            var randomNumbers = GenerateRandomNumbers();
+            var userNumberList = userNumbers.Split(',').Select(int.Parse).ToList();
+
+            var model = new PowerballModel
+            {
+                RandomNumbers = randomNumbers,
+                UserNumbers = userNumberList,
+                UserWon = userNumberList.Intersect(randomNumbers).Any()
+            };
+
+            return View("Index", model);
+        }
+
+        private List<int> GenerateRandomNumbers()
+        {
+            Random random = new Random();
+            return Enumerable.Range(1, 5).Select(_ => random.Next(1, 100)).ToList();
+        }
+    }
+}
---
+++ Models/PowerballModel.cs
@@ -0,0 +1,11 @@
+﻿// PowerballModel.cs
+namespace Powerball
+{
+    public class PowerballModel
+    {
+        public List<int> RandomNumbers { get; set; }
+        public List<int> UserNumbers { get; set; }
+        public bool UserWon { get; set; }
+    }
+}
+
